8ca3ddeb58fca39a21699130ff0b1a463072076d
Axel Grufman <Axel.Grufman@elev.ga.ntig.se>
2024-03-04 19:26:24 +0100

    Before break again

---
+++ script.js
@@ -0,0 +1,28 @@
+function addtocart(BåtNummer)
+{
+    console.log(BåtNummer)
+    if(BåtNummer == 'Båt1')
+    {
+
+    }
+    if(BåtNummer == 'Båt2')
+    {
+
+    }
+    if(BåtNummer == 'Båt3')
+    {
+
+    }
+    if(BåtNummer == 'Båt4')
+    {
+
+    }
+    if(BåtNummer == 'Båt5')
+    {
+
+    }
+    if(BåtNummer == 'Båt6')
+    {
+
+    }
+}
