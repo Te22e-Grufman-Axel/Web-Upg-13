756517692765852997dc70c538a21453140e6948
Axel Grufman <Axel.Grufman@elev.ga.ntig.se>
2024-03-06 14:56:06 +0100

    Klar?

--- script.js
+++ script.js
@@ -3,26 +3,55 @@ function addtocart(BåtNummer)
     console.log(BåtNummer)
     if(BåtNummer == 'Båt1')
     {
-
+        document.getElementById("BåtDrop1").style.display="block";
     }
     if(BåtNummer == 'Båt2')
     {
-
+        document.getElementById("BåtDrop2").style.display="block";
     }
     if(BåtNummer == 'Båt3')
     {
-
+        document.getElementById("BåtDrop3").style.display="block";
     }
     if(BåtNummer == 'Båt4')
     {
-
+        document.getElementById("BåtDrop4").style.display="block";
     }
     if(BåtNummer == 'Båt5')
     {
-
+        document.getElementById("BåtDrop5").style.display="block";
     }
     if(BåtNummer == 'Båt6')
     {
+        document.getElementById("BåtDrop6").style.display="block";
+    }
+}
 
+function TabortFrånKorg(BåtNummer)
+{
+    console.log(BåtNummer)
+    if(BåtNummer == 'Båt1')
+    {
+        document.getElementById("BåtDrop1").style.display="none";
+    }
+    if(BåtNummer == 'Båt2')
+    {
+        document.getElementById("BåtDrop2").style.display="none";
+    }
+    if(BåtNummer == 'Båt3')
+    {
+        document.getElementById("BåtDrop3").style.display="none";
+    }
+    if(BåtNummer == 'Båt4')
+    {
+        document.getElementById("BåtDrop4").style.display="none";
+    }
+    if(BåtNummer == 'Båt5')
+    {
+        document.getElementById("BåtDrop5").style.display="none";
+    }
+    if(BåtNummer == 'Båt6')
+    {
+        document.getElementById("BåtDrop6").style.display="none";
     }
 }
